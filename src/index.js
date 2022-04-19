f541984c686a7d14302cce6260299835f7fbce39
MarcoM94 <voldrak94@gmail.com>
2022-04-19 19:13:52 +0200

    fix

--- src/index.js
+++ src/index.js
@@ -9,13 +9,11 @@ import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
     <StateProvider initialState={initialState} reducer={reducer}>
       <Router>
         <App />
       </Router>
     </StateProvider>
-  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
