f9baf8f960c28c76d6c162552341a18105c293d1
MarcoM94 <voldrak94@gmail.com>
2022-04-19 20:29:22 +0200

    fix

--- src/App.js
+++ src/App.js
@@ -7,10 +7,8 @@ function App() {
   return (
     <div className="App">
       <Routes>
-
         <Route path="/" element={<Home/>} />
         <Route path="/favourite" element={<Favourite/>} />
-
       </Routes>
       </div>
   );
